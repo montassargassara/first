5e7049a592b4b1c05017eb3f044fe74e0e29b8fa
montassargassara <montassar.gassara01@gmail.com>
2022-08-10 09:27:25 +0100

    monta

---
+++ import java.util.Scanner;.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        
+        static void mymethod(string fname) {
+            system.out.println(fname + "refsnes");
+        }
+        static boolean palidrome(String ch)
+        {
+            boolean res=false;
+            return res;
+        }
+        static boolean existe(String ch1,String ch2)
+        {
+            boolean res=false;
+            return res;
+        }
+        static int NBoccurence(String ch,String ch2)
+        {
+            int res=0;
+            return res;
+        }
+        public static void Main(string[] args){
+    scanner input = new scanner(system.in);
+    string ch1,ch2;
+    systeme.out.println("ch1:");
+    ch1=input.next();
+    systeme.out.println("ch2:");
+    ch2=input.next();
+    if palidrome(ch1)
+    system.out.println(ch1 + "est un palindrome");
+    else
+    system.out.println(ch2 + "n'est pas un palindrome")
+    palidrome(ch2);
+    existe(ch1;ch2);
+    NBoccurence(ch1;ch2);
+    if palidrome(ch1)
+        }
+    }
+    
+}
